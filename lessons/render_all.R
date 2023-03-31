616cd0dfcdd333f2175b03bcae175c78fc5fb622
Hollister <Hollister.Jeff@epa.gov>
2023-03-31 12:03:25 -0400

    new render

--- lessons/render_all.R
+++ lessons/render_all.R
@@ -1,18 +1,26 @@
-render_all<-function(path=".",pattern="*.Rmd", ignore = NULL){
-  files <- list.files(path,pattern,full.names = T)
+render_all<-function(path=".",pattern=c("*.Rmd", "*.qmd"), ignore = NULL){
+  files <- NULL
+  for(i in pattern){
+    files <- c(files, list.files(path,i,full.names = T))
+  }
   if(!is.null(ignore)){
     
     files <- files[!grepl(ignore, files)]
   }
   
   for(i in files){
-    out <- stringr::str_replace(i,"Rmd","md")
-    if(!file.exists(out)){
-      knitr::knit(i,output=out)
-    } else if((file.info(i)$mtime-file.info(out)$mtime)>0){
-      knitr::knit(i,output=out)
+    if(grepl(".qmd", i)){
+      quarto::quarto_render(i)
+    } else {
+      out <- stringr::str_replace(i,"Rmd","md")
+      if(!file.exists(out)){
+        knitr::knit(i,output=out)
+      } else if((file.info(i)$mtime-file.info(out)$mtime)>0){
+        knitr::knit(i,output=out)
+      }
     }
   }
+  
   system("git add -A")
   system('git commit -m "new render"')
   system("git push origin main")
